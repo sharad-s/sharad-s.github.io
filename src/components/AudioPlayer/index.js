46038742af03f399f80bdd8a71eaeeaa67066dae
sharad-s <sharadshekar@gmail.com>
2020-07-14 22:30:24 -0500

    audio fixes

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -8,8 +8,18 @@ const initSrc =
 
 export const AudioPlayer = React.forwardRef((props, ref) => {
   const [audioURLs] = useState([
-    // initSrc,
-    "https://a.tumblr.com/tumblr_pyo5kf51M41r4pgk7o1.mp3"
+    // {
+    //   name: 'initSrc',
+    //   url: initSrc
+    // },
+    {
+      name: "drift",
+      url: "https://a.tumblr.com/tumblr_qa7jfcv7741r4pgk7o1.mp3",
+    },
+    {
+      name: 'plata',
+      url: "https://a.tumblr.com/tumblr_pyo5kf51M41r4pgk7o1.mp3"
+    }
     // "https://a.tumblr.com/tumblr_ohylne7XrN1r4pgk7o1.mp3",
     // "https://a.tumblr.com/tumblr_qa7jfcv7741r4pgk7o1.mp3",
     // "https://a.tumblr.com/tumblr_pyo5kf51M41r4pgk7o1.mp3",
@@ -19,34 +29,38 @@ export const AudioPlayer = React.forwardRef((props, ref) => {
   const [index, setIndex] = useState(0)
   const [src, setSrc] = useState(audioURLs[index])
 
-  const getURL = () => {
-    console.log("pre", index)
-    if (index + 1 >= audioURLs.length) {
-      setIndex(1)
-    } else {
-      setIndex(index + 1)
-    }
-    console.log("post", index)
-    // setIndex()
-    return audioURLs[index]
-  }
-
   const handleEnded = () => {
     const audio = ref.current
-    setSrc(getURL())
-    audio.src = src
+
+    // Index mgmt
+    let i = index;
+    if (i + 1 >= audioURLs.length) {
+      i = 1
+    } else {
+      i++
+    }
+
+    const source = audioURLs[i]
+
+    // Set it
+    setIndex(i)
+    setSrc(source)
+
+    console.log('setting source as', source.name)
+    audio.src = source.url
+    console.log('audio.load()')
     audio.load()
+    console.log('audio.play()')
     audio.play()
   }
 
   return (
     <audio
-      src={src}
+      src={src.url}
       controls
       crossOrigin="anonymous"
       ref={ref}
-      autoPlay={true}
-      style={{ zIndex: 100 }}
+      // style={{ zIndex: 100 }}
       onEnded={handleEnded}
       autoPlay={true}
       style={{ display: "none" }}
